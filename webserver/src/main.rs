5e9898ea0afa914003f44cce482a7528abefe65c
jongbum.park <bungae539@gmail.com>
2024-06-06 08:11:34 +0000

    싱글 스레드 웹서버 만들어보기
    [개발 및 변경 사항]
     - 첫 커밋에서 추가한 웹 서버 프로젝트에 싱글 스레드 서버 생성하도록 소스 수정
     - 에러 페이지 및 테스트 페이지 추가
    
    [변경된 파일 또는 모듈]
     - webserver 0.1.1-dev
    
    [개발자 테스트 내용]
    웹서버 구동 후 접속해보기
    
    cargo run (웹서버 구동)
    
    브라우저에서 서버 ip:port 입력 후 접속
    
    테스트 페이지, 에러 페이지 잘 표시되는지 확인

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,33 @@
+use std::{
+    fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_req = buf_reader.lines().next().unwrap().unwrap();
+
+    let http_split: Vec<_> = http_req.split(' ').collect();
+
+    let (status_line, filename) = if http_split[1] == "/" {
+        ("HTTP/1.1 200 OK", "test.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContents-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
